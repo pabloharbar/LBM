4aac6636d3815debe9d80b97bc9620cbcef3548b
pabloharbar <harbar2705@gmail.com>
2025-10-07 17:55:41 -0300

    LBM domain object

--- solver/models/domain.py
+++ solver/models/domain.py
@@ -4,30 +4,15 @@ import numpy as np
 
 
 @dataclass
-class DomainBox:
-    x_min: int
-    x_max: int
-    y_min: int
-    y_max: int
-    z_min: int
-    z_max: int
-
-    # TODO: Validate class so that *_max >= *_min
-
-
 class LBMDomain:
-    domain_box: DomainBox
+    points_coordinates: tuple[np.ndarray, np.ndarray, np.ndarray]
+    size = list[int]
+    n_dim: int
 
-    def __init__(
-        self, x_min: int, x_max: int, y_min: int, y_max: int, z_min: int, z_max: int
-    ):
-        self.domain_box = DomainBox(x_min, x_max, y_min, y_max, z_min, z_max)
-        self.domain_x = np.arange(x_min, x_max, 1)
-        self.domain_y = np.arange(y_min, y_max, 1)
-        self.domain_z = np.arange(z_min, z_max, 1)
-        self.domain_nodes = np.meshgrid(
-            self.domain_x,
-            self.domain_y,
-            self.domain_z,
-            indexing="ij",  # X then Y then Z
+    def __init__(self, domain_sizes: list[int]):
+        self.n_dim = len(domain_sizes)
+        self.size = domain_sizes
+        x, y, z = (np.arange(0, domain_sizes[i], 1) for i in range(3))
+        self.points_coordinates = np.meshgrid(
+            x, y, z, indexing="ij"  # X then Y then Z
         )[::-1]
