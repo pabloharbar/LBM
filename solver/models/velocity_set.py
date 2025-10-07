bcaf59ec5733c8718a8d78e310bdc0f69dafa6e5
pabloharbar <harbar2705@gmail.com>
2025-10-07 17:55:49 -0300

    velocity set objects

--- solver/models/velocity_set.py
+++ solver/models/velocity_set.py
@@ -1,30 +1,177 @@
+from dataclasses import dataclass
 from enum import Enum
 
+import numpy as np
 
-class VelocitySetTypes(Enum):
+
+class VelocitySetType(Enum):
     D2Q9 = "D2Q9"
+    D3Q19 = "D3Q19"
+    D3Q27 = "D3Q27"
 
 
-class BaseVelocitySet:
-    vel_set: VelocitySetTypes
+@dataclass(frozen=True)
+class VelocitySet:
+    set_type: VelocitySetType
+    order: int
+    directions: np.ndarray  # Shape: (Q, D)
+    weights: np.ndarray  # Shape: (Q,)
+    cs: float
 
-    def get_vel_set(self):
-        raise NotImplementedError()
 
+class VelocitySetFactory:
+    _sets = {
+        VelocitySetType.D2Q9: {
+            "cs": 1 / np.sqrt(3),
+            "order": 2,
+            "directions": np.array(
+                [
+                    [0, 0, 0],
+                    [1, 0, 0],
+                    [0, 1, 0],
+                    [-1, 0, 0],
+                    [0, -1, 0],
+                    [1, 1, 0],
+                    [-1, 1, 0],
+                    [-1, -1, 0],
+                    [1, -1, 0],
+                ]
+            ),
+            "weights": np.array(
+                [4 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 36, 1 / 36, 1 / 36, 1 / 36]
+            ),
+        },
+        VelocitySetType.D3Q19: {
+            "cs": 1 / np.sqrt(3),
+            "order": 2,
+            "directions": np.array(
+                [
+                    [0, 0, 0],
+                    [1, 0, 0],
+                    [-1, 0, 0],
+                    [0, 1, 0],
+                    [0, -1, 0],
+                    [0, 0, 1],
+                    [0, 0, -1],
+                    [1, 1, 0],
+                    [-1, 1, 0],
+                    [1, -1, 0],
+                    [-1, -1, 0],
+                    [1, 0, 1],
+                    [-1, 0, 1],
+                    [1, 0, -1],
+                    [-1, 0, -1],
+                    [0, 1, 1],
+                    [0, -1, 1],
+                    [0, 1, -1],
+                    [0, -1, -1],
+                ]
+            ),
+            "weights": np.array(
+                [
+                    1 / 3,
+                    1 / 18,
+                    1 / 18,
+                    1 / 18,
+                    1 / 18,
+                    1 / 18,
+                    1 / 18,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                    1 / 36,
+                ]
+            ),
+        },
+        VelocitySetType.D3Q27: {
+            "cs": 1 / np.sqrt(3),
+            "order": 3,
+            "directions": np.array(
+                [
+                    [0, 0, 0],
+                    [1, 0, 0],
+                    [-1, 0, 0],
+                    [0, 1, 0],
+                    [0, -1, 0],
+                    [0, 0, 1],
+                    [0, 0, -1],
+                    [1, 1, 0],
+                    [-1, 1, 0],
+                    [1, -1, 0],
+                    [-1, -1, 0],
+                    [1, 0, 1],
+                    [-1, 0, 1],
+                    [1, 0, -1],
+                    [-1, 0, -1],
+                    [0, 1, 1],
+                    [0, -1, 1],
+                    [0, 1, -1],
+                    [0, -1, -1],
+                    [1, 1, 1],
+                    [-1, 1, 1],
+                    [1, -1, 1],
+                    [1, 1, -1],
+                    [-1, -1, 1],
+                    [-1, 1, -1],
+                    [1, -1, -1],
+                    [-1, -1, -1],
+                ]
+            ),
+            "weights": np.array(
+                [
+                    8 / 27,
+                    2 / 27,
+                    2 / 27,
+                    2 / 27,
+                    2 / 27,
+                    2 / 27,
+                    2 / 27,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 54,
+                    1 / 216,
+                    1 / 216,
+                    1 / 216,
+                    1 / 216,
+                    1 / 216,
+                    1 / 216,
+                    1 / 216,
+                    1 / 216,
+                ]
+            ),
+        },
+    }
 
-class VelocitySetD2Q9:
-    vel_set = VelocitySetTypes.D2Q9
+    @staticmethod
+    def create(set_name: str) -> VelocitySet:
+        available_sets = [k.value for k in VelocitySetFactory._sets.keys()]
+        if set_name not in available_sets:
+            raise ValueError(
+                f"Velocity set '{set_name}' is not implemented, availables are {available_sets}."
+            )
+        set_type = VelocitySetType(set_name)
+        config = VelocitySetFactory._sets[set_type]
 
-    def get_vel_set(self):
-        Nx = 1
-        return {
-            "e0": {"direction": [0, 0, 0], "weight": 16 / 36, "index_offset": 0},
-            "e1": {"direction": [1, 0, 0], "weight": 4 / 36, "index_offset": 1},
-            "e2": {"direction": [0, 1, 0], "weight": 4 / 36, "index_offset": Nx},
-            "e3": {"direction": [-1, 0, 0], "weight": 4 / 36, "index_offset": -1},
-            "e4": {"direction": [0, -1, 0], "weight": 4 / 36, "index_offset": -Nx},
-            "e5": {"direction": [1, 1, 0], "weight": 1 / 36, "index_offset": Nx + 1},
-            "e6": {"direction": [-1, 1, 0], "weight": 1 / 36, "index_offset": Nx - 1},
-            "e7": {"direction": [-1, -1, 0], "weight": 1 / 36, "index_offset": -Nx - 1},
-            "e8": {"direction": [1, -1, 0], "weight": 1 / 36, "index_offset": -Nx + 1},
-        }
+        return VelocitySet(
+            set_type=set_type,
+            cs=config["cs"],
+            directions=config["directions"],
+            weights=config["weights"],
+        )
