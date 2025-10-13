0690ac317934abc39db8bbccd32ce9da55b5fa35
pabloharbar <harbar2705@gmail.com>
2025-10-13 19:00:43 -0300

    export 3d

--- solver/simulation/export.py
+++ solver/simulation/export.py
@@ -8,22 +8,26 @@ from solver.simulation.data import SimulationData
 
 
 def export_macros(
-    timestep: int,
     data: SimulationData,
     domain: LBMDomain,
+    target_path: pathlib.Path,
 ):
-    grid = pv.StructuredGrid(*domain.points_coordinates)
+    grid = pv.StructuredGrid(
+        domain.points_coordinates[0],
+        domain.points_coordinates[1],
+        domain.points_coordinates[2],
+    )
     grid.point_data["rho"] = data.rho.ravel()
     grid.point_data["ux"] = data.ux.ravel()
     grid.point_data["uy"] = data.uy.ravel()
     grid.point_data["uz"] = data.uz.ravel()
-    output_filename = pathlib.Path("./cases") / f"macros_{timestep:07d}.vts"
-    grid.save(output_filename, binary=True)
+    grid.save(target_path, binary=True)
 
 
-def export_population(timestep: int, data: SimulationData, domain: LBMDomain):
+def export_population(
+    data: SimulationData, domain: LBMDomain, target_path: pathlib.Path
+):
     grid = pv.StructuredGrid(*domain.points_coordinates)
     for i in range(data.f.shape[2]):
-        grid.point_data[f"f{i}"] = data.f[:, :, i].ravel()
-    output_filename = pathlib.Path("./cases") / f"population_{timestep:07d}.vts"
-    grid.save(output_filename, binary=True)
+        grid.point_data[f"f{i}"] = data.f[:, :, :, i].ravel()
+    grid.save(target_path, binary=True)
